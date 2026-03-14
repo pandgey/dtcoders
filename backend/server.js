4652f4e446c86df837d351b7b64dfc4b92d44b31
Michael <raisumig15@gmail.com>
2026-03-14 14:42:00 +1100

    add basic javascript

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,29 @@ app.listen(PORT_NUMBER, () => {
     console.log(`Server is running on port ${PORT_NUMBER}`);
 });
 
+/**
+ * Allow the user to upload a file on the homepage, helper functions will determine the height of a building and the number of buildings in the index.html file to be added and rendered.
+ */
 app.get("/", (req, res) => {
     res.render("index.html", { data: db1 });
 });
+
+function calculateBuildingHeight(file) {
+    let height = 0;
+    for (let i = 0; i < file.length; i++) {
+        if (file[i] === "1") {
+            height++;
+        }
+    }
+    return height;
+}
+
+function calculateNumberOfBuildings(file) {
+    let count = 0;
+    for (let i = 0; i < file.length; i++) {
+        if (file[i] === "1") {
+            count++;
+        }
+    }
+    return count;
+}
